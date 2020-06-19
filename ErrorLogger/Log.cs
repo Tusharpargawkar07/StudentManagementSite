cc955e5fc74dc485fb917ef2ad0870e66f25bf65
Tushar Pargawkar <Tusharpargawkar7@gmail.com>
2020-06-19 22:38:07 +0530

    Ninject Issue

---
+++ ErrorLogger/Log.cs
@@ -0,0 +1,41 @@
+﻿using log4net;
+using log4net.Config;
+using System;
+
+
+namespace Logger
+{
+    public class Log : ILogger
+    {
+        private readonly ILog log; 
+        public Log(string type="Test")
+        {
+            log = LogManager.GetLogger(type);
+            XmlConfigurator.Configure();
+        }
+        public void DebugLog(Exception ex)
+        {
+            log.Debug( ex + "\n");
+        }
+
+        public void ErrorLog(Exception ex)
+        {
+            log.Error( ex + "\n");
+        }
+
+        public void FatalLog(Exception ex)
+        {
+            log.Fatal( ex + "\n");
+        }
+
+        public void InfoLog(Exception ex)
+        {
+            log.Info( ex + "\n");
+        }
+
+        public void WarnLog(Exception ex)
+        {
+            log.Warn( ex + "\n");
+        }
+    }
+}
